99f6f1c69f22494ed62cbec0e6b35f4c3de1d588
Mohid123 <muhammadmohid141@gmail.com>
2024-01-01 22:33:11 +0500

    firebase config attached

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { CoreModule } from 'src/@core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from "src/environments/environment";
 
 @NgModule({
   declarations: [
@@ -21,6 +27,11 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     TuiRootModule,
     TuiDialogModule,
     TuiAlertModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,16 @@
 export const environment = {
   production: false,
   appVersion: 'v1.0.00',
-  apiUrl: 'https://api.animetography-blog.com',
+  apiUrl: '',
+  firebase: {
+    apiKey: "AIzaSyBuvMVh6ZCweTWFK9eYzUqfacR0up2y9Io",
+    authDomain: "animetography-blog.firebaseapp.com",
+    projectId: "animetography-blog",
+    storageBucket: "animetography-blog.appspot.com",
+    messagingSenderId: "619564651416",
+    appId: "1:619564651416:web:95ccf483830814286e51ed",
+    measurementId: "G-3GEGYLNELR"
+  },
   appThemeName: 'Animetography'
 };
 
